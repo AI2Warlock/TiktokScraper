6527b1af525225c1c80852b6c835b4aeb71d74b4
Brandon Ang <angn9096@gmail.com>
2025-02-10 20:57:04 +0800

    Update TiktokScraper.java

--- appinventor/components/src/ph/bxtdev/TiktokScraper/TiktokScraper.java
+++ appinventor/components/src/ph/bxtdev/TiktokScraper/TiktokScraper.java
@@ -7,6 +7,7 @@ import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
 import com.google.appinventor.components.runtime.ComponentContainer;
 import com.google.appinventor.components.runtime.EventDispatcher;
+import com.google.appinventor.components.runtime.util.*;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.net.HttpURLConnection;
