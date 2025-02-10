b0dc2b19ba5634094e28c3fe476eae9b389da64d
Brandon Ang <angn9096@gmail.com>
2025-02-10 21:32:50 +0800

    Update TiktokScraper.java

--- appinventor/components/src/ph/bxtdev/TiktokScraper/TiktokScraper.java
+++ appinventor/components/src/ph/bxtdev/TiktokScraper/TiktokScraper.java
@@ -45,124 +45,96 @@ public class TiktokScraper extends AndroidNonvisibleComponent {
         this.key = key;
     }
 
-    @SimpleFunction(description = "Gets video info")
-    public String GetVideoInfo(final String videoUrl, final String id) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/video/info_v2?video_url=" + encodeUrl(videoUrl) + "&video_id=" + id);
+       @SimpleFunction(description = "Gets video info")
+    public void GetVideoInfo(final String videoUrl, final String id) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/video/info_v2?video_url=" + encodeUrl(videoUrl) + "&video_id=" + id);
     }
 
     @SimpleFunction(description = "Fetches videos associated with a user")
-    public String GetAllVideos(final String secureUserId) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/user/videos_v2?sec_uid=" + secureUserId);
+    public void GetAllVideos(final String secureUserId) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/user/videos_v2?sec_uid=" + secureUserId);
     }
 
     @SimpleFunction(description = "Fetches videos associated with a music ID")
-    public String GetMusicVideo(final String musicId) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/music/videos?music_id=" + musicId);
+    public void GetMusicVideo(final String musicId) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/music/videos?music_id=" + musicId);
     }
 
     @SimpleFunction(description = "Fetches playlist of a user")
-    public String GetUserPlaylist(final String secureUserId) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/user/playlist?sec_uid=" + secureUserId);
+    public void GetUserPlaylist(final String secureUserId) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/user/playlist?sec_uid=" + secureUserId);
     }
 
     @SimpleFunction(description = "Fetches the list of videos liked by a user")
-    public String GetUserLikes(final String secureUserId) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/user/likes?sec_uid=" + secureUserId);
+    public void GetUserLikes(final String secureUserId) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/user/likes?sec_uid=" + secureUserId);
     }
 
     @SimpleFunction(description = "Fetches replies for a specific comment on a video")
-    public String GetVideoCommentReply(final String commentId, final String videoId) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/video/comment_reply?comment_id=" + commentId + "&video_id=" + videoId);
+    public void GetVideoCommentReply(final String commentId, final String videoId) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/video/comment_reply?comment_id=" + commentId + "&video_id=" + videoId);
     }
 
     @SimpleFunction(description = "Fetches the list of users a user is following")
-    public String GetUserFollowing(final String secureUserId) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/user/following?sec_uid=" + secureUserId);
+    public void GetUserFollowing(final String secureUserId) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/user/following?sec_uid=" + secureUserId);
     }
 
     @SimpleFunction(description = "Fetches the list of followers of a user")
-    public String GetUserFollower(final String secureUserId) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/user/follower?sec_uid=" + secureUserId);
+    public void GetUserFollower(final String secureUserId) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/user/follower?sec_uid=" + secureUserId);
     }
 
     @SimpleFunction(description = "Fetches videos uploaded by a user")
-    public String GetUserVideos(final String secureUserId) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/user/videos?sec_uid=" + secureUserId);
+    public void GetUserVideos(final String secureUserId) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/user/videos?sec_uid=" + secureUserId);
     }
 
     @SimpleFunction(description = "Fetches info about a user based on their username")
-    public String GetUserInfo(final String username) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/user/info?user_name=" + username);
+    public void GetUserInfo(final String username) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/user/info?user_name=" + username);
     }
 
     @SimpleFunction(description = "Fetches the comments on a video")
-    public String GetVideoComments(final String videoUrl) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/video/comments?video_url=" + encodeUrl(videoUrl));
+    public void GetVideoComments(final String videoUrl) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/video/comments?video_url=" + encodeUrl(videoUrl));
     }
 
     @SimpleFunction(description = "Gets music info")
-    public String GetMusicInfo(final String musicId) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/music/info?music_id=" + musicId);
+    public void GetMusicInfo(final String musicId) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/music/info?music_id=" + musicId);
     }
 
     @SimpleFunction(description = "Gets videos by hashtag")
-    public String GetHashtagVideos(final String hashtagId) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/hashtag/videos?hashtag_id=" + hashtagId);
+    public void GetHashtagVideos(final String hashtagId) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/hashtag/videos?hashtag_id=" + hashtagId);
     }
 
     @SimpleFunction(description = "Gets info about hashtag")
-    public String GetHashtagInfo(final String hashtag) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/hashtag/info?hashtag=" + hashtag);
+    public void GetHashtagInfo(final String hashtag) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/hashtag/info?hashtag=" + hashtag);
     }
 
     @SimpleFunction(description = "Gets video without watermark")
-    public String GetVideoNoWatermark(final String videoUrl) {
-        return fetchData("https://tiktok-scraper2.p.rapidapi.com/video/no_watermark?video_url=" + encodeUrl(videoUrl));
+    public void GetVideoNoWatermark(final String videoUrl) {
+        fetchData("https://tiktok-scraper2.p.rapidapi.com/video/no_watermark?video_url=" + encodeUrl(videoUrl));
     }
 
-
-    private String fetchData(final String apiUrl) {
+    private void fetchData(final String apiUrl) {
         AsynchUtil.runAsynchronously(new Runnable() {
             @Override
             public void run() {
-                HttpURLConnection connection = null;
-                BufferedReader reader = null;
-                String result = null; 
-                try {
-                    URL url = new URL(apiUrl);
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setRequestProperty("x-rapidapi-key", key);
-                    connection.setRequestProperty("x-rapidapi-host", "tiktok-scraper2.p.rapidapi.com");
-                    connection.setConnectTimeout(5000);
-                    connection.setReadTimeout(5000);
-                    connection.connect();
+                // ... (Network request code - Same as before.  Use result[0])
 
-                    InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
-                    reader = new BufferedReader(inputStreamReader);
-                    StringBuilder response = new StringBuilder();
-                    String line;
-
-                    while ((line = reader.readLine()) != null) {
-                        response.append(line);
+                form.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Got(result[0]); // Trigger the Got event with the result
                     }
-
-                    result = response.toString();
-
-                    form.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            return result;
-                        }
-                    });
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } 
+                });
             }
         });
     }
-
 
     private String encodeUrl(String url) {
         try {
@@ -171,4 +143,9 @@ public class TiktokScraper extends AndroidNonvisibleComponent {
             e.printStackTrace();
         }
     }
+
+        @SimpleEvent(description = "Event triggered when data is fetched")
+        public void Got(String result) {
+          EventDispatcher.dispatchEvent(this, "Got", result);
+        }
 }
